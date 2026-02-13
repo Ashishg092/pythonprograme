15edb5d742f9a10667ef017a409a16e5860985d6
Ashishg092 <momentsm092@gmail.com>
2026-02-13 13:15:53 +0530

    Add files via upload

---
+++ exp2a.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 13 12:53:25 2026
+
+@author: User
+"""
+n = int ( input (" Enter Number : "))
+
+fact = 1 
+
+for i in range (1 , n + 1):
+    fact = fact * i
+    
+    print ( " factorial : " , fact)
